16f42bcf3aec253ad38c2c134631c4a6f013b37a
Henadzi Bessonov <bessonov.henadzi@gmail.com>
2023-12-18 12:36:26 +0300

    update lecture

--- src/test/java/com/bessonov/lesson1/Main.java
+++ src/test/java/com/bessonov/lesson1/Main.java
@@ -2,6 +2,35 @@ package com.bessonov.lesson1;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        int x = (int) (Math.random() * 20);
+        System.out.println(x);
+    }
+
+    public static void sum(int oneNumber, int twoNumber) {
+        System.out.println("Введите первое число: ");
+        System.out.println("Введите второе число: ");
+        System.out.println("Результат сложения первого и второго числа = " + (oneNumber + twoNumber));
+    }
+
+    public static void printStartTimes(int times) {
+        while (times > 0) {
+            System.out.println('*');
+            times--;
+        }
+    }
+
+    public static void printMap() {
+        System.out.println("0 1 2 3");
+        for (int i = 0; i < 3; i++) {
+            System.out.print((i + 1) + " ");
+            for (int j = 0; j < 3; j++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+
+    public static void sumThree(int a, int b, int c) {
+        System.out.println(a + b + c);
     }
 }
